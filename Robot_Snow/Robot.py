5126929e60553ae25e0c704d5a9013b64351c4fd
Valery <valkirik1995@mail.ru>
2025-04-28 21:58:42 +0300

    created a class Robot

--- Robot_Snow/Robot.py
+++ Robot_Snow/Robot.py
@@ -1,3 +1,10 @@
+class Robot:
+    def __init__(self, name, x = 0, y = 0):
+        self.name = name
+        self.x = x
+        self.y = y
 
+    def __str__(self):
+        return f"Rpbot {self.name} is on the spot: x - {self.x}, y = {self.y}"
 
 
