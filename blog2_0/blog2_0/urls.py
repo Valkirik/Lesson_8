828043aa2a1c6df70e0663270ea083096ef94ff0
Valery <valkirik1995@mail.ru>
2025-05-31 00:10:33 +0300

    adding a simple veiw for exemple

--- blog2_0/blog2_0/urls.py
+++ blog2_0/blog2_0/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from news_feed.views import say_hello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index/', say_hello)
 ]
