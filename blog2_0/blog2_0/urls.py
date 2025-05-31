8e4f1e8bfb0b59cb794e2b8544dd3a7a311f01ff
Valery <valkirik1995@mail.ru>
2025-05-31 23:59:04 +0300

    working with creating functions and classes in view

--- blog2_0/blog2_0/urls.py
+++ blog2_0/blog2_0/urls.py
@@ -16,9 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from news_feed.views import say_hello
+from news_feed.views import say_hello, get_pages_list, PostListView, PostCreateView
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', say_hello)
+    path('index/', say_hello),
+    path('pages/', get_pages_list),
+    path('posts/', PostListView.as_view()),
+    path('post_create/', PostCreateView.as_view()),
 ]
+
+
--- blog2_0/news_feed/views.py
+++ blog2_0/news_feed/views.py
@@ -1,5 +1,29 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views.generic import ListView, CreateView
+
+from .models import Page, Post
 
 def say_hello(request):
     return HttpResponse("Hello")
+
+
+def get_pages_list(request):
+    context = {}
+    context["all_pages"] = Page.objects.all()
+    return render(request, "all_pages.html", context)
+
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = "all_posts.html"
+
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ["name", "content", "page"]
+    template_name = "post_create.html"
+    success_url = "/admin"
+
