6f05687aa26f9881c70ad2c2aa376867cddf7a6e
Valery <valkirik1995@mail.ru>
2025-06-07 17:39:42 +0300

    the class had been finished with migration

--- blog2_0/todo/models.py
+++ blog2_0/todo/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class TodoListItem(models.Model):
+    content = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.content}"
+
+    def __repr__(self):
+        return f"{self.pk} - {self.content}"
+
+    class Meta:
+        verbose_name = "Task"
+        verbose_name_plural = "Tasks"
+
